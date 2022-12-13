9d658b3b63299292fd8b52ef1be679d49197b3dd
Abylay <aurelioleclarke@gmail.com>
2022-12-13 14:42:33 +0600

    inited 1.0

---
+++ Stream Chat/Stream Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stream Chat
+//
+//  Created by Aurelio Le Clarke on 13.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
